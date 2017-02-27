67caf94e44dd635241338ce43790d157c807795d
Vaidas <kaveiki.nk@gmail.com>
2017-02-27 20:00:01 +0000

    Changed the algorithm to find the next * primes rather than all of the primes in the provided range

--- primeTest/src/primetest/PrimeTest.java
+++ primeTest/src/primetest/PrimeTest.java
@@ -28,23 +28,25 @@ public class PrimeTest {
         sc.close();
         
         ArrayList<Integer> primes = new ArrayList<Integer>(); // will be storing the primes in an ArrayList since it is dynamic
-        int counter = 0;                                //  This variable is used as a number counter
-        while(counter < numOfPrime){                    //  Condition to stop once the counter reaces the same number as the number specified by the users input
-            if(counter % 2 != 0 || counter == 2){       //  If the number is even it is not a prime except for 2
+        int numberCounter = 0;                                //  This variable is used as a number counter
+        int primeCounter = 0;
+        while(primeCounter  < numOfPrime){                    //  Condition to stop once the counter reaces the same number as the number specified by the users input
+            if(numberCounter % 2 != 0 || numberCounter == 2){       //  If the number is even it is not a prime except for 2
                 float counter2 = 1;                     //
                 float divCounter = 0;                   //
-                while(counter2 <= counter){             //  This loop is used to test if the number is only devisible by 1 and itself  
-                    if((counter % counter2 == 0)){      //  Tests if the number can be devided without remainder
+                while(counter2 <= numberCounter){             //  This loop is used to test if the number is only devisible by 1 and itself  
+                    if((numberCounter % counter2 == 0)){      //  Tests if the number can be devided without remainder
                         divCounter = divCounter + 1;    //  if it can it increments the counter
                     }                                   //
                     counter2 = counter2 + 2;            //  we only need to check the uneven numebrs
                 }                                       //
-                if(divCounter == 2 || counter == 2){    //  The only way divCounter is 2 is if the numeber has been devided twice which means it was devided by 1 and itself and ofcourse we need to allow for 2 
-                    primes.add(counter);                //  Adding the prime to an array
-                    System.out.println(counter);        //  Prints out the prime
+                if(divCounter == 2 || numberCounter == 2){    //  The only way divCounter is 2 is if the numeber has been devided twice which means it was devided by 1 and itself and ofcourse we need to allow for 2 
+                    primes.add(numberCounter);                //  Adding the prime to an array
+                    System.out.println(numberCounter);        //  Prints out the prime
+                    primeCounter++;
                 }
             }
-            counter++;                                  //  Increments the number 
+            numberCounter++;                                  //  Increments the number 
         }
         return primes;                                  //  Returns the final array of primes
     }
